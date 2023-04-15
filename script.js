cd60a3877beecbbba34a7983cdb51bc95f9d4f12
Ysum <93167785+YuxinSun0@users.noreply.github.com>
2023-04-14 22:49:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.getElementById("music").volume = 0;
-function sendTextMsg() {
-	cloud_textMsg = cloud_textMsg2;
-	cloud_textMsg2 = cloud_textMsg3;
-	cloud_textMsg3 = cloud_textMsg4;
-	cloud_textMsg4 = cloud_textMsg5;
-	cloud_textMsg5 = document.getElementById("textMsgInput").value;
-}
 function updateTextMsg() {
 	document.getElementById("msg").innerHTML = cloud_textMsg;
 	document.getElementById("msg2").innerHTML = cloud_textMsg2;
